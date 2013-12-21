c445904174f5e96cd28ec6f590bdaeb00bbce966
davenportw15 <willdavenport12@gmail.com>
2013-12-20 23:33:05 -0500

    Model for user management created at model/User.rb Currently allows for the creation or user and the request of user information from the database

---
+++ Paths.rb
@@ -0,0 +1,16 @@
+#defines all paths in application
+#uses class methods so that there is no need to instantiate it 
+class Paths
+	def self.root
+		Dir.pwd.match(/(.*)\/.*/)
+		return $1
+	end
+
+	def self.users
+		return self.root + "/model/Users.rb"
+	end
+
+	def self.database
+		return self.root + "/databases/store.db"
+	end
+end
---
+++ model/Users.rb
@@ -0,0 +1,56 @@
+require 'sqlite3'
+require_relative '../Paths.rb'
+
+class Users
+	def initialize
+		@db = SQLite3::Database.new(Paths.root + "/databases/store.db")
+		@error = SQLite3::SQLException
+	end
+
+	#formats user data into a hash; information can be called in view through OBJECT[:username], for example
+	def format(userData)
+		formatted = {}
+		formatted[:username] = userData[0] || nil
+		formatted[:password] = userData[1] || nil
+		formatted[:firstname] = userData[2] || nil
+		formatted[:lastname] = userData[3] || nil
+		return formatted
+	end
+
+	def userExists?(username)
+		if @db.execute("select username from users where username=='#{username}'").empty?
+			return false
+		else
+			return true
+		end
+	end
+
+	#use the params hash to specify which fields in the database should be targeted
+	def getUserByUsername(username)
+		if userExists?(username)	
+			return @db.execute("select * from users where username=='#{username}'").map do |userData|
+				format(userData)
+			end.first #.first removes unnecessary embeddding of information in an array
+		else
+			return false
+		end
+	end
+
+	def newUser(username, password, firstname, lastname)
+		if userExists?(username)
+			return false
+		else
+			@db.execute("insert into users ('username','password','firstname','lastname') values ('#{username}','#{password}','#{firstname}','#{lastname}')")
+			return true
+		end
+	end
+
+	def deleteUser(username)
+		if userExists?(username)
+			@db.execute("delete from users where username=='#{username}'")
+			return true
+		else
+			return false
+		end
+	end
+end
